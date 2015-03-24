07a13c3f8b3eb56fb2b3bc577efb42e46b7b8de4
Mark Mulder <markmulder@gmail.com>
2015-03-24 10:17:52 +0100

    wip

--- endpoints.go
+++ endpoints.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
-	"net"
 	"net/http"
 	"time"
 
@@ -18,26 +16,11 @@ type ResponseWriter interface {
 
 	// StatusCode returns the written status code, or 0 if none has been written yet.
 	StatusCode() int
-	// Written returns whether the header has been written yet.
-	Written() bool
-	// Size returns the size in bytes of the body written so far.
-	Size() int
 }
 
 type appResponseWriter struct {
 	http.ResponseWriter
 	statusCode int
-	size       int
-}
-
-// Don't need this yet because we get it for free:
-func (w *appResponseWriter) Write(data []byte) (n int, err error) {
-	if w.statusCode == 0 {
-		w.statusCode = http.StatusOK
-	}
-	size, err := w.ResponseWriter.Write(data)
-	w.size += size
-	return size, err
 }
 
 func (w *appResponseWriter) WriteHeader(statusCode int) {
@@ -46,34 +29,11 @@ func (w *appResponseWriter) WriteHeader(statusCode int) {
 }
 
 func (w *appResponseWriter) StatusCode() int {
+	if w.statusCode == 0 {
+		return 200
+	}
+
 	return w.statusCode
-}
-
-func (w *appResponseWriter) Written() bool {
-	return w.statusCode != 0
-}
-
-func (w *appResponseWriter) Size() int {
-	return w.size
-}
-
-func (w *appResponseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	hijacker, ok := w.ResponseWriter.(http.Hijacker)
-	if !ok {
-		return nil, nil, fmt.Errorf("the ResponseWriter doesn't support the Hijacker interface")
-	}
-	return hijacker.Hijack()
-}
-
-func (w *appResponseWriter) CloseNotify() <-chan bool {
-	return w.ResponseWriter.(http.CloseNotifier).CloseNotify()
-}
-
-func (w *appResponseWriter) Flush() {
-	flusher, ok := w.ResponseWriter.(http.Flusher)
-	if ok {
-		flusher.Flush()
-	}
 }
 
 func trackTime(start time.Time, name string) {
@@ -95,7 +55,6 @@ func requestLogger(h http.Handler) http.Handler {
 
 func handleFavicon(w http.ResponseWriter, r *http.Request) {
 	defer trackTime(time.Now(), "handleFavicon")
-	http.Redirect(w, r, "/markie", http.StatusFound)
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
